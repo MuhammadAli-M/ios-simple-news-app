b7e617dba7a71bbaebef9704b14e3200c6396ccd
Muhammad Adam <muhammadalyadam@gmail.com>
2020-12-09 10:09:07 +0200

    add-headlines Add the api to get headlines

---
+++ SimpleNewsApp/API/Headlines/HeadlinesService.swift
@@ -0,0 +1,50 @@
+//
+//  HeadlinesService.swift
+//  SimpleNewsApp
+//
+//  Created by Muhammad Adam on 12/9/20.
+//
+
+import Foundation
+
+
+class HeadlinesService: NewsService{
+    
+    static let shared = HeadlinesService()
+    private let endpoint = "/v2/top-headlines"
+    
+    // TODO: Make the request safe
+    func getHeadlines(countryCode: CountryCode, category: CategoryName, completionHandler: @escaping ( ([Article], Error?) -> Void)){
+        
+        let headlineUrl = HeadlinesURL(baseUrl: baseUrl + endpoint,
+                                       queries: [
+                                        HeadlineQuery(key: .country, value: countryCode),
+                                        HeadlineQuery(key: .category, value: category),
+                                        HeadlineQuery(key: .apiKey, value: apiKey)
+                                       ])
+        
+        
+        guard let url = headlineUrl.url else { return }
+
+        let task = URLSession.shared.dataTask(with: url) { (data, response, error) in
+            guard let data = data else{
+                errorLog("requesting \(url.absoluteString) , error : \(error?.localizedDescription ?? " ")")
+                completionHandler([],error)
+                return
+            }
+            
+            do{
+                let headlinesResponseData = try newJSONDecoder().decode(HeadlinesResponseData.self, from: data)
+                completionHandler(headlinesResponseData.articles ?? [],nil)
+                debugLog("headlinesResponseData is recieved")
+            }catch{
+                completionHandler([],error)
+                errorLog("while json parsing, error : \(error.localizedDescription)")
+            }
+        }
+        task.resume()
+    }
+}
+    
+
+
---
+++ SimpleNewsApp/API/Headlines/HeadlinesURL.swift
@@ -0,0 +1,41 @@
+//
+//  HeadlinesURL.swift
+//  SimpleNewsApp
+//
+//  Created by Muhammad Adam on 12/9/20.
+//
+
+import Foundation
+
+
+struct HeadlinesURL{
+    let baseUrl: String
+    let queries: [HeadlineQuery]
+    
+    var url: URL?{
+        guard let url = URL(string: baseUrl) else {return nil}
+        guard var components = URLComponents(url: url, resolvingAgainstBaseURL: false) else {return nil}
+        
+        components.queryItems = queries.map{
+            URLQueryItem(name: $0.key.rawValue, value: $0.value)
+        }
+        return components.url
+    }
+    
+}
+
+
+struct HeadlineQuery {
+    let key:HeadlineQueryKey
+    let value:String
+}
+
+enum HeadlineQueryKey: String {
+    case country
+    case category
+    case sources
+    case q
+    case pageSize
+    case page
+    case apiKey
+}
