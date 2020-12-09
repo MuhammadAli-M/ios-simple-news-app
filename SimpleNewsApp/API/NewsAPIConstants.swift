72471bf598037f1afc4016df6d461df2f164b79f
Muhammad Adam <muhammadalyadam@gmail.com>
2020-12-09 08:38:31 +0200

    add-headlines Add headlines service to get top headlines and api constants

---
+++ SimpleNewsApp/API/NewsAPIConstants.swift
@@ -0,0 +1,29 @@
+//
+//  APIConstants.swift
+//  SimpleNewsApp
+//
+//  Created by Muhammad Adam on 12/9/20.
+//
+
+import Foundation
+
+typealias CountryCode = String
+
+struct NewsAPIConstants {
+    
+    static let avaiableCountryCodes: [CountryCode] = ["ae", "ar", "at", "au", "be", "bg", "br", "ca", "ch", "cn", "co", "cu", "cz", "de", "eg", "fr", "gb", "gr", "hk", "hu", "id", "ie", "il", "in", "it", "jp", "kr", "lt", "lv", "ma", "mx", "my", "ng", "nl", "no", "nz", "ph", "pl", "pt", "ro", "rs", "ru", "sa", "se", "sg", "si", "sk", "th", "tr", "tw", "ua", "us", "ve", "za"]
+    
+}
+
+protocol Service {
+    var baseUrl:String { get }
+}
+
+protocol NewsService: Service {
+    var apiKey: String { get }
+}
+
+extension NewsService {
+    var baseUrl:String { "https://newsapi.org" }
+    var apiKey: String { "278f755dac054ab098bb406be3b4e2f2"}
+}
